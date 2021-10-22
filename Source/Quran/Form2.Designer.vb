45d68eccc2bc7b9e08350a797074c23cc36ceb4a
Dante720a <65083076+Dante720a@users.noreply.github.com>
2021-10-22 12:54:06 +0330

    Add files via upload

---
+++ Source/Quran/Form2.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SoreSelect = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SoreRemain = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LoadLists = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SoreSelect
+        '
+        Me.SoreSelect.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SoreSelect.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer))
+        Me.SoreSelect.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.SoreSelect.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.SoreSelect.ForeColor = System.Drawing.Color.FromArgb(CType(CType(187, Byte), Integer), CType(CType(178, Byte), Integer), CType(CType(161, Byte), Integer))
+        Me.SoreSelect.FormattingEnabled = True
+        Me.SoreSelect.ItemHeight = 18
+        Me.SoreSelect.Location = New System.Drawing.Point(104, 26)
+        Me.SoreSelect.Name = "SoreSelect"
+        Me.SoreSelect.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.SoreSelect.Size = New System.Drawing.Size(162, 324)
+        Me.SoreSelect.TabIndex = 71
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Button1.Location = New System.Drawing.Point(272, 112)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(63, 31)
+        Me.Button1.TabIndex = 72
+        Me.Button1.Text = "< افزودن"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Button2.Location = New System.Drawing.Point(8, 112)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 31)
+        Me.Button2.TabIndex = 73
+        Me.Button2.Text = "انتقال به بالا"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Button3.Location = New System.Drawing.Point(8, 154)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(90, 31)
+        Me.Button3.TabIndex = 74
+        Me.Button3.Text = "انتقال به پاین"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'SoreRemain
+        '
+        Me.SoreRemain.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SoreRemain.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer))
+        Me.SoreRemain.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.SoreRemain.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.SoreRemain.ForeColor = System.Drawing.Color.FromArgb(CType(CType(187, Byte), Integer), CType(CType(178, Byte), Integer), CType(CType(161, Byte), Integer))
+        Me.SoreRemain.FormattingEnabled = True
+        Me.SoreRemain.ItemHeight = 18
+        Me.SoreRemain.Location = New System.Drawing.Point(341, 26)
+        Me.SoreRemain.Name = "SoreRemain"
+        Me.SoreRemain.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.SoreRemain.Size = New System.Drawing.Size(162, 324)
+        Me.SoreRemain.TabIndex = 75
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label1.Location = New System.Drawing.Point(62, -19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label1.Size = New System.Drawing.Size(119, 16)
+        Me.Label1.TabIndex = 77
+        Me.Label1.Text = "لیست علافه مندیها:"
+        '
+        'LoadLists
+        '
+        Me.LoadLists.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LoadLists.Location = New System.Drawing.Point(8, 5)
+        Me.LoadLists.Name = "LoadLists"
+        Me.LoadLists.Size = New System.Drawing.Size(75, 23)
+        Me.LoadLists.TabIndex = 78
+        Me.LoadLists.Text = "LoadLists"
+        Me.LoadLists.UseVisualStyleBackColor = True
+        Me.LoadLists.Visible = False
+        '
+        'Button6
+        '
+        Me.Button6.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button6.Font = New System.Drawing.Font("Tahoma", 9.75!)
+        Me.Button6.Location = New System.Drawing.Point(272, 154)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(63, 31)
+        Me.Button6.TabIndex = 79
+        Me.Button6.Text = "حذف >"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button8.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Button8.Location = New System.Drawing.Point(8, 315)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(90, 31)
+        Me.Button8.TabIndex = 81
+        Me.Button8.Text = "ذخیره تغییرات"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label2.Location = New System.Drawing.Point(407, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label2.Size = New System.Drawing.Size(99, 16)
+        Me.Label2.TabIndex = 82
+        Me.Label2.Text = "لیست سوره ها:"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label3.Location = New System.Drawing.Point(124, 5)
+        Me.Label3.Name = "Label3"
+        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label3.Size = New System.Drawing.Size(152, 16)
+        Me.Label3.TabIndex = 83
+        Me.Label3.Text = "لیست سوره های منتخب:"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(89, Byte), Integer), CType(CType(89, Byte), Integer), CType(CType(89, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(515, 362)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.LoadLists)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SoreRemain)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.SoreSelect)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form2"
+        Me.Text = "فهرست سوره های منتخب"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SoreSelect As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents SoreRemain As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents LoadLists As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+End Class
---
+++ Source/Quran/Form2.vb
@@ -0,0 +1,214 @@
+﻿Imports System.Data.OleDb
+
+
+
+Public Class Form2
+    '##\\ Connect to Access Database \\#################################################################################################
+    Private con As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\Quran.mdb")
+    Dim cmd As OleDb.OleDbCommand
+    Dim da As OleDb.OleDbDataAdapter
+    Dim dt As DataTable
+    Dim sql As String
+    Dim maxrow As Integer
+
+    Private Function get_maxrow(ByVal sql)
+        Try
+            con.Open()
+            cmd = New OleDb.OleDbCommand
+            da = New OleDb.OleDbDataAdapter
+            dt = New DataTable
+            With cmd
+                .Connection = con
+                .CommandText = sql
+            End With
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            maxrow = dt.Rows.Count
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            con.Close()
+            da.Dispose()
+        End Try
+        Return maxrow
+    End Function
+
+    Private Sub LoadLists_Click(sender As Object, e As EventArgs) Handles LoadLists.Click
+        SoreRemain.Items.Clear()
+        Try
+            con.Open()
+            cmd = New OleDb.OleDbCommand
+            da = New OleDb.OleDbDataAdapter
+            dt = New DataTable
+            sql = "Select * from TheQuran_List where note2 IS NULL"
+            With cmd
+                .Connection = con
+                .CommandText = sql
+            End With
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            For i = 0 To dt.Rows.Count - 1
+
+                SoreRemain.Items.Add(dt.Rows(i).Item(1))
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        Finally
+            con.Close()
+            da.Dispose()
+        End Try
+
+        SoreSelect.Items.Clear()
+        Try
+            con.Open()
+            cmd = New OleDb.OleDbCommand
+            da = New OleDb.OleDbDataAdapter
+            dt = New DataTable
+            sql = "Select * from TheQuran_List where note2 IS not NULL ORDER BY note2 asc"
+            With cmd
+                .Connection = con
+                .CommandText = sql
+            End With
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            For i = 0 To dt.Rows.Count - 1
+                SoreSelect.Items.Add(dt.Rows(i).Item(1))
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        Finally
+            con.Close()
+            da.Dispose()
+        End Try
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        With SoreSelect
+4:
+            If .SelectedIndex > 0 Then
+5:
+                Dim i = .SelectedIndex - 1
+6:
+                .Items.Insert(i, .SelectedItem)
+7:
+                .Items.RemoveAt(.SelectedIndex)
+8:
+                .SelectedIndex = i
+9:
+            End If
+10:
+        End With
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        With SoreSelect
+17:
+            If .SelectedIndex < .Items.Count - 1 Then
+18:
+                Dim i = .SelectedIndex + 2
+19:
+                .Items.Insert(i, .SelectedItem)
+20:
+                .Items.RemoveAt(.SelectedIndex)
+21:
+                .SelectedIndex = i - 1
+22:
+            End If
+23:
+        End With
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SoreSelect.Items.Add(SoreRemain.SelectedItem.ToString)
+        SoreRemain.Items.Remove(SoreRemain.SelectedItem.ToString)
+        SoreSelect.SelectedIndex = SoreSelect.Items.Count - 1
+    End Sub
+
+
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        '##\\ Resume From ‌Bookmarked \\#################################################################################################
+        Dim a, b, c, d As String
+        d = SoreSelect.Text
+        a = Split(SoreSelect.Text, ")")(0).ToString
+        b = a.Length
+        c = a.Substring(1, a.Length - 1)
+
+        Dim clean As New OleDbCommand("UPDATE TheQuran_List SET note2 = NULL WHERE SorehNo = " & c, con)
+        con.Open()
+        clean.ExecuteNonQuery()
+        con.Close()
+        LoadLists_Click(Nothing, Nothing)
+
+
+        With SoreRemain
+            .SelectedIndex = -1 '// -1 Index ='s No.Item.Selected.
+
+            For Each itm As String In .Items '// loop thru items.
+                If itm.ToLower = d.ToLower Then '// Case inSensitive search.
+                    .SelectedItem = itm '// select item.
+                    Exit For '// exit loop since item located. 
+                End If
+            Next
+            Exit Sub '// just.because the next 2 lines are not worth reading. :)
+
+        End With
+
+
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs)
+
+        SoreSelect.Items.Add(SoreRemain.SelectedItem.ToString)
+        SoreRemain.Items.Remove(SoreRemain.SelectedItem.ToString)
+        SoreSelect.SelectedIndex = SoreSelect.Items.Count - 1
+
+        Dim a, b, c As String
+        a = Split(SoreSelect.Text, ")")(0).ToString
+        b = a.Length
+        c = a.Substring(1, a.Length - 1)
+
+        Dim clean As New OleDbCommand("UPDATE TheQuran_List SET note2 = " & SoreSelect.SelectedIndex & " WHERE SorehNo = " & c, con)
+        con.Open()
+        clean.ExecuteNonQuery()
+        con.Close()
+        LoadLists_Click(Nothing, Nothing)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim i As Double
+        Dim a, b, c As String
+
+        With SoreSelect
+            .SelectedIndex = -1 '// -1 Index ='s No.Item.Selected.
+
+            For Each itm As String In .Items '// loop thru items.
+                i = i + 1
+                a = Split(itm, ")")(0).ToString
+                b = a.Length
+                c = a.Substring(1, a.Length - 1)
+
+                Dim clean As New OleDbCommand("UPDATE TheQuran_List SET note2 = " & i & " WHERE SorehNo = " & c, con)
+                con.Open()
+                clean.ExecuteNonQuery()
+                con.Close()
+
+            Next
+            Dim reset As New OleDbCommand("UPDATE LastAye SET FavSooreh = 1 , FavAyeh = 1", con)
+            con.Open()
+            reset.ExecuteNonQuery()
+            con.Close()
+            Me.Close()
+            Exit Sub '// just.because the next 2 lines are not worth reading. :)
+
+        End With
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadLists_Click(Nothing, Nothing)
+    End Sub
+End Class
